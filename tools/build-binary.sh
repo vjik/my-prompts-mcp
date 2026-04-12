b807f9d69c2cfdead51fc9083d44d982119a5f2d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-04-12 10:20:33 +0300

    improve

--- tools/build-binary.sh
+++ tools/build-binary.sh
@@ -4,6 +4,7 @@ set -eu
 IMAGE=${DOCKER_IMAGE:-ghcr.io/vjik/my-prompts-mcp-dev:latest}
 ROOT_DIR=$(cd "$(dirname "$0")/.." && pwd)
 DOCKER_RUN_PARAMS="--rm -v $ROOT_DIR:/app --user $(id -u):$(id -g)"
+BINARY_NAME=${BINARY_NAME:-my-prompts-mcp}
 
 docker run $DOCKER_RUN_PARAMS $IMAGE box compile
 
@@ -13,5 +14,5 @@ docker run $DOCKER_RUN_PARAMS \
   $IMAGE \
   spc micro:combine \
     --with-micro=/builder/buildroot/bin/micro.sfx \
-    --output=/app/build/my-prompts-mcp \
+    --output="/app/build/$BINARY_NAME" \
     /app/build/my-prompts-mcp.phar
