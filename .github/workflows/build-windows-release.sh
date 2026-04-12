292bd5a5a99646c14d478f596fea2f08853d8321
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-04-12 18:55:15 +0300

    Windows release building

---
+++ .github/workflows/build-windows-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eu
+
+# Box
+curl -Lf -o /usr/local/bin/box https://github.com/box-project/box/releases/download/4.7.0/box.phar
+chmod +x /usr/local/bin/box
+
+# static-php-cli
+mkdir -p /tmp/build-tools/static-php-cli
+cd /tmp/build-tools/static-php-cli
+git clone https://github.com/crazywhalecc/static-php-cli.git --depth=1 .
+composer install
+chmod +x bin/spc
+ln -s /tmp/build-tools/static-php-cli/bin/spc /usr/local/bin/spc
+
+# Build micro.sfx
+mkdir /tmp/builder && cd /tmp/builder
+spc download php-src,micro --with-php=8.5.4 --for-libs=zlib --prefer-pre-built
+spc doctor --auto-fix
+spc build --build-micro 'phar'
+
+# Build PHAR
+cd "$GITHUB_WORKSPACE"
+composer install --no-dev --optimize-autoloader
+box compile
+
+# Combine binary
+spc micro:combine \
+  --with-micro=/tmp/builder/buildroot/bin/micro.sfx \
+  --output="$GITHUB_WORKSPACE/build/$BINARY_NAME" \
+  "$GITHUB_WORKSPACE/build/my-prompts-mcp.phar"
